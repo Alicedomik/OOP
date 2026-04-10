e85657c14ccfe5b5967ae64603c5ca751d1f9ddb
Alicedomik <alicedomnik6@gmail.com>
2026-04-10 17:20:52 +0300

    commit 1

--- lab4/4.4.1.py
+++ lab4/4.4.1.py
@@ -51,17 +51,30 @@ class Hand:
         self.t.forward(self.length)
         self.t.penup()
 
-class Clock:
+
+class Watch:
     def __init__(self):
+        self.t = turtle.Turtle()
+    @staticmethod
+    def get_current_time():
+        return datetime.datetime.now().time()
+    def update(self):
+        pass
+    def run(self):
+        self.update()
+
+class AnalogWatch(Watch):
+    def __init__(self):
+        super().__init__()
         turtle.tracer(0)
         self.face = ClockFace(150)
         self.hour_hand = Hand(70, "black", 4)
         self.min_hand = Hand(110, "black", 4)
         self.sec_hand = Hand(130, "red", 2)
-        self.face.setup(turtle.Turtle())
+        self.face.setup(self.t)
 
     def update(self):
-        now = datetime.datetime.now()
+        now = self.get_current_time()
         s_angle = now.second * 6
         m_angle = now.minute * 6 + now.second * 0.1
         h_angle = (now.hour % 12) * 30 + now.minute * 0.5
@@ -71,7 +84,38 @@ class Clock:
         turtle.update()
         turtle.ontimer(self.update, 1000)
 
-if __name__ == '__main__':
-    c = Clock()
-    c.update()
-    turtle.done()
+class DigitalWatch(Watch):
+    def __init__(self, x=0, y=0, format = "24", color="black"):
+        super().__init__()
+        self.format = format
+        self.t.hideturtle()
+        self.t.penup()
+        self.t.goto(x, y)
+        self.t.color(color)
+
+    def update(self):
+        now = self.get_current_time()
+        hour, minute, second = now.hour, now.minute, now.second
+        if self.format == "12":
+            if hour < 12:
+                suf = " AM"
+            else:
+                suf = " PM"
+            display_h = hour % 12
+            if display_h == 0:
+                display_h = 12
+            time_str = f"{display_h:02d}:{minute:02d}:{second:02d}{suf}"
+        else:
+            time_str = f"{hour:02d}:{minute:02d}:{second:02d}"
+        self.t.clear()
+        self.t.write(time_str, align="center", font=("Courier", 80, "bold"))
+        turtle.ontimer(self.update, 1000)
+
+
+a = AnalogWatch()
+a.update()
+turtle.done()
+
+# d = DigitalWatch()
+# d.run()
+# turtle.done()
